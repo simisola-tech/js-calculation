ebb05b9ffa0285adf51a26e1e259e4c98e6639b6
omoniyi omosimisola <omoniyiomosimisola18@gmail.com>
2024-10-08 13:50:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+alert('This is a simple arithmetic by Omoniyi Omosimisola Priscilla')
+    
+    document.write(' BUILDING A SIMPLE ARITHMETIC USING JAVASCRIPT <br>')
+    document.write('<br>')
+    document.write('<br>')
+    document.write('<br>')
+
+    //Addition
+    const egg = 30;
+    let yam = 25;
+    let food = egg + yam;
+
+    document.write('The food tray is ' + food)
+    document.write('<br>')
+    document.write('<br>')
+
+    //subtraction
+    const boys  = 50;
+    let girls = 10;
+    var pupils = boys - girls;
+
+    document.write('The total number of pupils in the class are ' + pupils)
+    document.write('<br>')
+    document.write('<br>')
+
+    //Division
+    var simi = 300;
+    let sola = 10;
+    let names = simi / sola;
+
+    document.write('The total number of names are ' + names)
+    document.write('<br>')
+    document.write('<br>')
+
+    // Modulus
+    const min = 2;
+    let hours = 20;
+    var time = min % hours;
+
+    document.write('The period of time is ' + time)
+    document.write('<br>')
+    document.write('<br>')
+
+    // Raise to power
+    var denominator = 2;
+    var numerator = 4;
+    var respond = denominator ** numerator;
+
+    document.write('Answer ' + respond)
